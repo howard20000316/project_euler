56a8c7f66fabdf07a1268cb8117810630fce2b2a
howard20000316 <howard890316@gmail.com>
2022-02-01 02:24:10 +0800

    Create p65.m

---
+++ p65.m
@@ -0,0 +1,30 @@
+eth = [];
+ethc = 1;
+for i = 1 : 100
+    eth(i) = ethc;
+    if ethc == 1 && i > 2 && eth(i - 1) == 1 || i == 1
+        ethc = 2 * (floor(i / 3) + 1);
+    elseif ethc == 1
+        ethc = 1;
+    else
+        ethc = 1;
+    end
+end
+temp = 0;
+numm = 99;
+for i = 1 : numm
+    if temp == 0
+        temp = sym(1 / eth(numm + 1 - i));
+    else
+        temp = 1 / (eth(numm + 1 - i) + temp);
+    end
+end
+[n, d] = numden(2 + temp);
+n = char(n);
+answer = 0;
+for i = 1 : length(n)
+    answer = answer + str2num(n(i));
+end
+disp(answer)
+
+
